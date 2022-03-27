cc0b7fe271d7d754a8a0a735d4583ce997760a5f
Patryk Czarnik (win) <p.czarnik@alx.pl>
2022-03-27 09:40:34 +0200

    eksport do csv - bardziej rozbudowany przykład

--- PC13-Maven/src/main/java/przyklad_csv/Eksport.java
+++ PC13-Maven/src/main/java/przyklad_csv/Eksport.java
@@ -1,6 +1,5 @@
 package przyklad_csv;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -15,10 +14,19 @@ import org.apache.commons.csv.CSVPrinter;
 public class Eksport {
 
 	public static void main(String[] args) {
+		final String sql = "SELECT employee_id, first_name, last_name, job_title,"
+				+ " lower(email) || '@alx.pl' AS email, replace(phone_number, '.', '') AS tel,"
+				+ " salary, hire_date, department_name, street_address AS address, postal_code, city, country_name AS country"
+				+ " FROM employees"
+				+ " LEFT JOIN jobs USING(job_id)"
+				+ " LEFT JOIN departments USING(department_id)"
+				+ " LEFT JOIN locations USING(location_id)"
+				+ " LEFT JOIN countries USING(country_id)"
+				+ " ORDER BY employee_id";
+
 		try(Connection c = DriverManager.getConnection("jdbc:postgresql://localhost/hr", "kurs", "abc123")) {
-            try (PreparedStatement stmt = c.prepareStatement("SELECT * FROM employees ORDER BY employee_id")) {
+			try (PreparedStatement stmt = c.prepareStatement(sql)) {
                 try (ResultSet rs = stmt.executeQuery()) {
-                	
                 	zapiszCSV(rs, "eksport.csv");
                 }
             }
@@ -27,20 +35,19 @@ public class Eksport {
         }
 	}
 
-	private static void zapiszCSV(ResultSet rs, String plik) {
-		CSVFormat format = CSVFormat.EXCEL;
-		
+	private static void zapiszCSV(ResultSet rs, String plik) throws SQLException {
+		CSVFormat format = CSVFormat.Builder.create()
+				.setDelimiter(';')
+				.setHeader(rs)
+				.setQuote('"')
+				.build();
+				
 		try(PrintWriter out = new PrintWriter(plik);
 			CSVPrinter printer = new CSVPrinter(out, format)) {
 			
 			printer.printRecords(rs);
 			System.out.println("Gotowe");
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
