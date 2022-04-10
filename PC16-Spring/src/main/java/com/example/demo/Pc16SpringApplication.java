b4a7ec85d8b5a811e6757d43f4cd8b12e1a8aafb
Patryk Czarnik <patryk@patrykczarnik.net>
2022-04-10 09:43:08 +0200

    Nowy projekt Spring Boot

---
+++ PC16-Spring/src/main/java/com/example/demo/Pc16SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pc16SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pc16SpringApplication.class, args);
+	}
+
+}
