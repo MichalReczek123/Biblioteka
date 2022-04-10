4990b78af0098d342e4525e0d0ff9bd5b623ec3e
Patryk Czarnik <patryk@patrykczarnik.net>
2022-04-10 14:06:39 +0200

    Przykład generator - początek

---
+++ PC16-Spring/src/main/java/com/example/demo/GeneratorController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GeneratorController {
+	
+	@RequestMapping("/generator")
+	public String generuj(Integer zakres, Integer ile, Model model) {
+		Random random = new Random();
+		List<Integer> liczby = new LinkedList<>();
+		for (int i = 0; i < ile; i++) {
+			liczby.add(random.nextInt(zakres));
+		}
+		model.addAttribute("liczby", liczby);
+		return "generator.html";
+	}
+	
+}
